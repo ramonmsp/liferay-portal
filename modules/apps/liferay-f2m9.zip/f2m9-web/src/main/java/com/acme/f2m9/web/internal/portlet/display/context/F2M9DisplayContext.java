c00683166212bd0b54f151969df8f4f036c5f8d8
Rafael Praxedes <rafael.praxedes@liferay.com>
2021-10-14 15:41:14 -0300

    retrieve the workflowInstanceId for todos into a workflow process

---
+++ modules/apps/liferay-f2m9.zip/f2m9-web/src/main/java/com/acme/f2m9/web/internal/portlet/display/context/F2M9DisplayContext.java
@@ -0,0 +1,33 @@
+package com.acme.f2m9.web.internal.portlet.display.context;
+
+import com.acme.f2m9.model.Todo;
+import com.liferay.portal.kernel.model.WorkflowInstanceLink;
+import com.liferay.portal.kernel.service.WorkflowInstanceLinkLocalService;
+import org.osgi.service.component.annotations.Reference;
+
+import javax.portlet.RenderResponse;
+import javax.servlet.http.HttpServletRequest;
+
+public class F2M9DisplayContext {
+
+	public F2M9DisplayContext(
+		WorkflowInstanceLinkLocalService workflowInstanceLinkLocalService) {
+
+		_workflowInstanceLinkLocalService = workflowInstanceLinkLocalService;
+	}
+
+	public Long getWorkflowInstanceId(Todo todo) {
+		WorkflowInstanceLink workflowDefinitionLink =
+			_workflowInstanceLinkLocalService.fetchWorkflowInstanceLink(
+				todo.getCompanyId(), todo.getGroupId(), Todo.class.getName(),
+				todo.getPrimaryKey());
+
+		if(workflowDefinitionLink == null) {
+			return null;
+		}
+
+		return workflowDefinitionLink.getWorkflowInstanceId();
+	}
+
+	private final WorkflowInstanceLinkLocalService _workflowInstanceLinkLocalService;
+}
