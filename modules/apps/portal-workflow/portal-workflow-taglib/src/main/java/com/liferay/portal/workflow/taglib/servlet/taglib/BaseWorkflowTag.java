9cd5dea0b8d0a30b9bf5901b3fa2a711e8dd29f5
Feliphe Marinho <feliphe.marinho@liferay.com>
2021-10-18 17:21:50 -0300

    LPS-140733 Add BaseWorkflowTag

---
+++ modules/apps/portal-workflow/portal-workflow-taglib/src/main/java/com/liferay/portal/workflow/taglib/servlet/taglib/BaseWorkflowTag.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.workflow.taglib.servlet.taglib;
+
+import com.liferay.portal.workflow.taglib.internal.context.util.WorkflowStatusTaglibHelper;
+import com.liferay.portal.workflow.taglib.internal.servlet.ServletContextUtil;
+import com.liferay.taglib.util.IncludeTag;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.PageContext;
+
+/**
+ * @author Feliphe Marinho
+ */
+public abstract class BaseWorkflowTag extends IncludeTag {
+
+	@Override
+	public int doStartTag() throws JspException {
+		setAttributeNamespace(WorkflowStatusTaglibHelper.ATTRIBUTE_NAMESPACE);
+
+		return super.doStartTag();
+	}
+
+	@Override
+	public void setPageContext(PageContext pageContext) {
+		super.setPageContext(pageContext);
+
+		setServletContext(ServletContextUtil.getServletContext());
+	}
+
+	@Override
+	protected void cleanUp() {
+		super.cleanUp();
+	}
+
+}
