9bbd364105e6b2ba6161fdb59876675c0c2e1e04
ramon <ramon.maciel@liferay.com>
2021-10-19 11:12:07 -0300

    LRDOCS-9702 Create a react component to call Instance Tracker

---
+++ modules/apps/liferay-f2m9.zip/f2m9-web/src/main/resources/META-INF/resources/js/Index.js
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ */
+
+import ClayLink from '@clayui/link';
+import ClayModal, {useModal} from '@clayui/modal';
+import WorkflowInstanceTracker from '@liferay/portal-workflow-instance-tracker-web/js/components/WorkflowInstanceTracker';
+import React, {useState} from 'react';
+
+export default function Index({workflowInstanceId}) {
+	const [showInstanceTrackerModal, setShowInstanceTrackerModal] = useState(
+		false
+	);
+
+	const {observer} = useModal({
+		onClose: () => {
+			setShowInstanceTrackerModal(false);
+		},
+	});
+
+	return (
+		<>
+			<ClayLink onClick={() => setShowInstanceTrackerModal(true)}>
+				{Liferay.Language.get('track-workflow')}
+			</ClayLink>
+
+			{showInstanceTrackerModal && (
+				<ClayModal observer={observer} size="full-screen">
+					<ClayModal.Header>
+						{Liferay.Language.get('track-workflow')}
+					</ClayModal.Header>
+
+					<ClayModal.Body>
+						<WorkflowInstanceTracker
+							workflowInstanceId={workflowInstanceId}
+						/>
+					</ClayModal.Body>
+				</ClayModal>
+			)}
+		</>
+	);
+}
