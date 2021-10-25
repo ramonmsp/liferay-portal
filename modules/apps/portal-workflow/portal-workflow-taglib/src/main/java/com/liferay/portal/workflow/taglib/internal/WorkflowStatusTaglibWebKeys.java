44782b889347ec0d4793fdf44e4107f0df69886c
Feliphe Marinho <feliphe.marinho@liferay.com>
2021-10-25 18:58:04 -0300

    LPS-140733 Create WorkflowStatusTaglibWebKeys

---
+++ modules/apps/portal-workflow/portal-workflow-taglib/src/main/java/com/liferay/portal/workflow/taglib/internal/WorkflowStatusTaglibWebKeys.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.workflow.taglib.internal;
+
+/**
+ * @author Feliphe Marinho
+ */
+public class WorkflowStatusTaglibWebKeys {
+
+	public static final String BEAN = "bean";
+
+	public static final String HELP_MESSAGE = "helpMessage";
+
+	public static final String ID = "id";
+
+	public static final String MODEL = "model";
+
+	public static final String SHOW_HELP_MESSAGE = "showHelpMessage";
+
+	public static final String SHOW_STATUS_LABEL = "showStatusLabel";
+
+	public static final String STATUS = "status";
+
+	public static final String STATUS_MESSAGE = "statusMessage";
+
+	public static final String VERSION = "version";
+
+}
